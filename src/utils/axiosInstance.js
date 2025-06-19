34e04086e317181e9d30e8f514acba811eed38c4
Miaad Asjad <miaadasjad1514@gmail.com>
2025-06-19 18:08:23 +0500

    Updated API base URL

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000';
+const baseURL = import.meta.env.VITE_API_BASE_URL;
+
 
 if (import.meta.env.DEV) {
   console.log('🌐 API Base URL:', baseURL);
